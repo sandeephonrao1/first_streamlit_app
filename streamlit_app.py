e8b693aeddcdf43be96171b5adf0e9b9e9e9705f
sandeephonrao1 <152753052+sandeephonrao1@users.noreply.github.com>
2023-12-03 07:46:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner')
+
+st.header('Breakfast Menu')
+
+# List of Breakfast items
+breakfast_items = ['Idli', 'Pohe', 'Vada Sambhar', 'Chai', 'Coffee', 'Dosa']
+
+# Display the list of Breakfast items
+for item in breakfast_items:
+    st.write(f'- {item}')
+
