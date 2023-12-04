e4ec005d017b88ec4516cb183da290a761ab9f96
sandeephonrao1 <152753052+sandeephonrao1@users.noreply.github.com>
2023-12-04 18:11:15 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
+
+
+import streamlit
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie') streamit.text('Hard-Boiled Free-Range Egg')
